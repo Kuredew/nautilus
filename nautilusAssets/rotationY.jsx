2cddd094e9380a1c64f19ee4d35ea757df9065dc
Kuredew <burrohmanhabib35@gmail.com>
2026-01-24 23:44:20 +0700

    feat: add wiggle feature for position, rotation, and scale

--- nautilusAssets/rotationY.jsx
+++ nautilusAssets/rotationY.jsx
@@ -1,6 +1,14 @@
 // For Rotation
 if (!ctrlValue) { ctrlValue = 0 }
 
+if (ctrlIsWiggleRotation) {
+  var ctrlWiggleRotationAmp = ctrlFx("Wiggle Rot Amp").value
+  var ctrlWiggleRotationFreq = ctrlFx("Wiggle Rot Freq").value
+
+  seedRandom(ctrlFx("Wiggle Rot Seed").value + index)
+  ctrlValue += (wiggle(ctrlWiggleRotationFreq, ctrlWiggleRotationAmp) - value)
+}
+
 var ctrlRotationStrength = ctrlFx("Rotation Strength").valueAtTime(realTime - delay);
 
 if (ctrlIsSeparateRotation) {
