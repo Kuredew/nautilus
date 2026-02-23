176c1b6545a1ab033e1ed9e7490763b74d80e276
Kuredew <burrohmanhabib35@gmail.com>
2026-02-23 20:30:23 +0700

    fix: Difficult to reduce the scale of letters in the text layer

--- nautilusAssets/text/scaleMask.jsx
+++ nautilusAssets/text/scaleMask.jsx
@@ -1,12 +1,16 @@
 try {
-  var ctrlFx = effect("NautiFlow");
   var maskInTangent = thisLayer.mask("Mask 1").maskPath.inTangents()[textIndex - 1];
   var maskOutTangent = thisLayer.mask("Mask 1").maskPath.outTangents()[textIndex - 1];
 
-  var tangentsLength = length(maskInTangent) + length(maskOutTangent)
-  var maxScale = ctrlFx("Max Scale");
+  var s = thisProperty.propertyGroup(3).property.scale;
 
-  (tangentsLength / maxScale) * 100;
+  var from = (100 / (s[0] - 100)) * -100;
+  var to = 100;
+
+  var tangentsLength = (length(maskInTangent) + length(maskOutTangent)) / 2;
+  var finals = linear(tangentsLength, 0, 100, from, to);
+
+	[finals, finals]
 } catch (e) {
   [100, 100]
 }
