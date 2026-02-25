ca5cf7da4b66fdf882cb3d86bbd975cb9b557cfe
Kuredew <burrohmanhabib35@gmail.com>
2026-02-25 22:46:29 +0700

    style: Separate nautilus function error from global handleError function

--- Nautilus.jsx
+++ Nautilus.jsx
@@ -837,14 +837,18 @@ function NautilusScript(ui_ref) {
 
   load()
   utils.createPalette(ui_ref);
+
+  function handleError(msg) {
+    alert("One function of Nautilus gives an Error.\n\nDetail:\n" + msg + "\n\nYou can open an issue at\nhttps://github.com/Kuredew/nautilus\nif the problem persist.");
+  }
 }
 
-function handleError(msg) {
-  alert("Nautilus encountered Error! : \n" + msg + "\n\n" + "if you have a problem, please let me know by contacting me on instagram (@zeanlost)  \n\nNautilus is made by Kureichi");
+function handleLoadError(msg) {
+  alert("There was a problem loading Nautilus into After Effects.\n\nDetail:\n" + msg + "\n\nOpen an issue at\nhttps://github.com/Kuredew/nautilus\nif the problem persist.")
 }
 
 try {
   NautilusScript(this);
 } catch (e) {
-  handleError(e.message)
+  handleLoadError(e.message)
 }
