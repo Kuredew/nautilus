1c3656871890b13d2a6a8b5f711b4e5f871a3000
Kureichi <165907455+Kuredew@users.noreply.github.com>
2026-07-09 18:26:35 +0700

    Revert "refactor: simplify library loading"

--- src/utils/expression.js
+++ src/utils/expression.js
@@ -1,17 +1,16 @@
 // defaultVariableExpression, propertyExpression, nautilusEffectName, ownCompName, compName, nullName, effectNameList
 export function getExpr(template, replaceConfig) {
   try {
-    if (!template) {
-      throw new Error("Expression is null or undefined");
+    let expr = template
+    
+    for(const key in replaceConfig) {
+      expr = expr.replace(key, replaceConfig[key])      
     }
-
-    let expr = template;
-    for (const key in replaceConfig) {
-      expr = expr.replace(key, replaceConfig[key]);
-    }
-
-    return expr;
+    
+    return expr
   } catch (e) {
-    throw new Error("[getExpr] " + template);
+    throw new Error("[getExpr] " + e.message)
   }
 }
+
+
