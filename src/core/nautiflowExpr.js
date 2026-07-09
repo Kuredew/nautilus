6a8cddbe3e3f19f10809853fcfc291c0b6dd60e7
Kureichi <165907455+Kuredew@users.noreply.github.com>
2026-07-09 18:27:27 +0700

    refactor: revert "refactor: simplify library loading" (#74)

--- src/core/nautiflowExpr.js
+++ src/core/nautiflowExpr.js
@@ -1,57 +1,49 @@
-import { nautilus } from "../state";
-import { getExpr } from "../utils/expression";
-import { addAnimatorWithExprs } from "./textLayer";
+import { nautilus } from "../state"
+import { getExpr } from "../utils/expression"
+import { addAnimatorWithExprs } from "./textLayer"
 
 export function applyTextLayer(textLayer) {
-  const rawSelectorExprsKey = [
-    "positionMask",
-    "rotationMask",
-    "scaleMask",
-    "trackingMask",
-  ];
-  const finalSelectorExprs = rawSelectorExprsKey.map((key) => {
-    const rawExpr = nautilus.expression.text[key];
-
-    if (!rawExpr) throw new Error(`Expression "${key}" is null or undefined`);
-
-    return getExpr(rawExpr, {
-      NAUTIFLOW_FX_NAME: nautilus.nautiFlowEffectName,
-    });
-  });
-
+  const rawSelectorExprs = [
+    nautilus.expression.text.positionMask,
+    nautilus.expression.text.rotationMask,
+    nautilus.expression.text.scaleMask,
+    nautilus.expression.text.trackingMask
+  ]
   const finalPropertyExprs = [
     nautilus.expression.text.positionMaskValue,
     nautilus.expression.text.rotationMaskValue,
     nautilus.expression.text.scaleMaskValue,
-    nautilus.expression.text.trackingMaskValue,
-  ];
+    nautilus.expression.text.trackingMaskValue
+  ]
 
+  const finalSelectorExprs = rawSelectorExprs.map((expr) => (getExpr(expr, { NAUTIFLOW_FX_NAME: nautilus.nautiFlowEffectName })))
+  
   const configs = [
-    {
-      name: "Nautiflow Position",
+    { 
+      name: "Nautiflow Position", 
       propertyName: "position",
-      propertyExpr: finalPropertyExprs[0],
-      selectorExpr: finalSelectorExprs[0],
+      propertyExpr: finalPropertyExprs[0], 
+      selectorExpr: finalSelectorExprs[0] 
     },
-    {
-      name: "Nautiflow Rotation",
+    { 
+      name: "Nautiflow Rotation", 
       propertyName: "rotation",
-      propertyExpr: finalPropertyExprs[1],
-      selectorExpr: finalSelectorExprs[1],
+      propertyExpr: finalPropertyExprs[1], 
+      selectorExpr: finalSelectorExprs[1] 
     },
-    {
-      name: "Nautiflow Scale",
+    { 
+      name: "Nautiflow Scale", 
       propertyName: "scale",
-      propertyExpr: finalPropertyExprs[2],
-      selectorExpr: finalSelectorExprs[2],
+      propertyExpr: finalPropertyExprs[2], 
+      selectorExpr: finalSelectorExprs[2] 
     },
-    {
-      name: "Nautiflow Tracking",
+    { 
+      name: "Nautiflow Tracking", 
       propertyName: "tracking",
-      propertyExpr: finalPropertyExprs[3],
-      selectorExpr: finalSelectorExprs[3],
+      propertyExpr: finalPropertyExprs[3], 
+      selectorExpr: finalSelectorExprs[3] 
     },
-  ];
+  ]
 
-  configs.forEach((config) => addAnimatorWithExprs(textLayer, config));
+  configs.forEach(config => addAnimatorWithExprs(textLayer, config))
 }
