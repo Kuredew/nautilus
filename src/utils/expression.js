95eb46f05324b78fd491ff2b07b3d32febe1a722
Kuredew <burrohmanhabib35@gmail.com>
2026-07-09 18:29:38 +0700

    fix: error message not displayed

--- src/utils/expression.js
+++ src/utils/expression.js
@@ -1,16 +1,14 @@
 // defaultVariableExpression, propertyExpression, nautilusEffectName, ownCompName, compName, nullName, effectNameList
 export function getExpr(template, replaceConfig) {
   try {
-    let expr = template
-    
-    for(const key in replaceConfig) {
-      expr = expr.replace(key, replaceConfig[key])      
+    let expr = template;
+
+    for (const key in replaceConfig) {
+      expr = expr.replace(key, replaceConfig[key]);
     }
-    
-    return expr
+
+    return expr;
   } catch (e) {
-    throw new Error("[getExpr] " + e.message)
+    throw new Error("[getExpr] " + e.message);
   }
 }
-
-
